8fcd88759f04941955b85f8aa96e99b9224984af
DodikFirmansahh <134833762+DodikFirmansahh@users.noreply.github.com>
2023-07-01 10:12:22 +0700

    Latihan1

---
+++ Latihan1/Latihan1.php
@@ -0,0 +1,20 @@
+<?php
+$Nilai1 = 10;
+$Nilai2 = 7;
+$BobotNilai1 = 2;
+$BobotNilai2 = 8;
+
+$TotalNilai1 = (10 * 2);
+$TotalNilai2 = (7 * 8);
+
+$TotalNilai = $TotalNilai1 + $TotalNilai2;
+
+$HasilNilai = $Nilai1 + $Nilai2;
+
+
+#Tampilkan data
+
+echo "Total nilai = {$TotalNilai} <br>";
+echo "Hasil dari 10 + 7 adalah : {$HasilNilai} <br>";
+echo "Nama : Dodik Firmansah <br>";
+echo "NIM : 121103018 <br>";
